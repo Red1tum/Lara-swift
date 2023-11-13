ef5bf88b4d676ad9254f7201a95572163628be8f
Reditum <fantomskn@gmail.com>
2023-11-13 07:08:21 -0800

    init

---
+++ Lara/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lara
+//
+//  Created by Reditum on 11/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack() {
+
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lara/LaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaraApp.swift
+//  Lara
+//
+//  Created by Reditum on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
